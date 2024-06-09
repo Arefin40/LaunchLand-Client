eaf69483a7e14ebb14475018e8d752cf56289038
ShahriarArefin <arfin17310@gmail.com>
2024-06-09 06:57:08 +0600

    implemented user-profile update system

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useAuth } from "@contexts/AuthContext";
+import { useUpdateUser, useLoggedUser } from "@hooks/useUser";
 import { Input } from "@components/Form";
 import Button from "@components/Button";
 import DashboardTitle from "@containers/DashboardTitle";
@@ -8,22 +8,21 @@ import { useEffect } from "react";
 
 const Profile = () => {
    const [editable, setEditable] = useState(false);
-   const { user, isAuthenticating } = useAuth();
-
-   const {
-      register,
-      reset,
-      handleSubmit,
-      formState: { errors },
-   } = useForm();
+   const { data: user } = useLoggedUser();
+   const updateInfoMutation = useUpdateUser(user?._id);
+   const { register, setValue, handleSubmit, formState: { errors } } = useForm(); // prettier-ignore
 
    useEffect(() => {
-      reset({
-         name: user?.displayName || "",
-         photoUrl: user?.photoURL || "",
-         email: user?.email || "",
-      });
+      if (user) {
+         setValue("name", user.name);
+         setValue("photoUrl", user.photoUrl);
+      }
    }, [user]);
+
+   const updateInfo = (data) => {
+      if (data.name !== user.name || data.photoUrl !== user.photoUrl)
+         updateInfoMutation.mutate(data);
+   };
 
    return (
       <section className="space-y-10">
@@ -31,7 +30,7 @@ const Profile = () => {
 
          <main className="w-full">
             <div className="w-full max-w-3xl grid gap-y-8 sm:gap-y-12">
-               <div>
+               <form onSubmit={handleSubmit(updateInfo)}>
                   <div className="flex items-center justify-between gap-y-1 sm:gap-4 sm:px-0 sm:items-center flex-wrap">
                      <div>
                         <h3 className="text-base font-semibold leading-7 text-gray-900">
@@ -43,6 +42,7 @@ const Profile = () => {
                      </div>
 
                      <Button
+                        type={editable ? "button" : "submit"}
                         onClick={() => setEditable((prev) => !prev)}
                         size="small"
                         variant="outlined"
@@ -53,86 +53,57 @@ const Profile = () => {
                   </div>
                   <div className="mt-6 border-t border-gray-100">
                      <div className="divide-y divide-gray-100">
-                        {isAuthenticating ? (
-                           <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
-                              <div className="w-3/4 h-6 bg-gray-100 rounded animate-pulse" />
-                              <div className="h-6 bg-gray-100 rounded animate-pulse" />
-                           </div>
-                        ) : (
-                           <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
-                              <label
-                                 htmlFor="name"
-                                 className="text-sm font-medium leading-6 text-gray-900"
-                              >
-                                 Full name
-                              </label>
-                              <Input
-                                 placeholder="Enter your name"
-                                 className="sm:col-span-2"
-                                 spellCheck={false}
-                                 hideBorder={!editable}
-                                 disabled={!editable}
-                                 errors={errors}
-                                 {...register("name", {
-                                    required: "Name is required",
-                                 })}
-                              />
-                           </div>
-                        )}
+                        <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
+                           <label
+                              htmlFor="name"
+                              className="text-sm font-medium leading-6 text-gray-900"
+                           >
+                              Full name
+                           </label>
+                           <Input
+                              placeholder="Enter your name"
+                              className="sm:col-span-2"
+                              spellCheck={false}
+                              hideBorder={!editable}
+                              disabled={!editable}
+                              errors={errors}
+                              {...register("name", {
+                                 required: "Name is required",
+                              })}
+                           />
+                        </div>
 
-                        {isAuthenticating ? (
-                           <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
-                              <div className="w-3/4 h-6 bg-gray-100 rounded animate-pulse" />
-                              <div className="sm:col-span-2 h-6 bg-gray-100 rounded animate-pulse" />
-                           </div>
-                        ) : (
-                           <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
-                              <label
-                                 htmlFor="photoUrl"
-                                 className="text-sm font-medium leading-6 text-gray-900"
-                              >
-                                 Photo url
-                              </label>
-                              <Input
-                                 type="url"
-                                 placeholder="Enter your photo url"
-                                 className="sm:col-span-2"
-                                 errors={errors}
-                                 hideBorder={!editable}
-                                 disabled={!editable}
-                                 {...register("photoUrl", {
-                                    required: "Photo url is required",
-                                 })}
-                              />
-                           </div>
-                        )}
+                        <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
+                           <label
+                              htmlFor="photoUrl"
+                              className="text-sm font-medium leading-6 text-gray-900"
+                           >
+                              Photo url
+                           </label>
+                           <Input
+                              type="url"
+                              placeholder="Enter your photo url"
+                              className="sm:col-span-2"
+                              errors={errors}
+                              hideBorder={!editable}
+                              disabled={!editable}
+                              {...register("photoUrl", {
+                                 required: "Photo url is required",
+                              })}
+                           />
+                        </div>
 
-                        {isAuthenticating ? (
-                           <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
-                              <div className="w-3/4 h-6 bg-gray-100 rounded animate-pulse" />
-                              <div className="h-6 bg-gray-100 rounded animate-pulse" />
-                           </div>
-                        ) : (
-                           <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
-                              <label className="text-sm font-medium leading-6 text-gray-900">
-                                 Email address
-                              </label>
-                              <Input
-                                 placeholder="Enter your email"
-                                 className="sm:col-span-2"
-                                 spellCheck={false}
-                                 errors={errors}
-                                 hideBorder={!editable}
-                                 disabled={!editable}
-                                 {...register("email", {
-                                    required: "Email is required",
-                                 })}
-                              />
-                           </div>
-                        )}
+                        <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
+                           <h3 className="text-sm font-medium leading-6 text-gray-900">
+                              Email address
+                           </h3>
+                           <p className="mt-1 px-3 max-w-2xl text-sm leading-6 text-gray-900">
+                              {user?.email}
+                           </p>
+                        </div>
                      </div>
                   </div>
-               </div>
+               </form>
 
                <div>
                   <div>
@@ -143,21 +114,13 @@ const Profile = () => {
                   </div>
                   <div className="mt-6 border-t border-gray-100">
                      <div className="divide-y divide-gray-100">
-                        {isAuthenticating ? (
-                           <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
-                              <div className="w-3/4 h-6 bg-gray-100 rounded animate-pulse" />
-                              <div className="h-6 bg-gray-100 rounded animate-pulse" />
-                           </div>
-                        ) : (
-                           <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
-                              <label className="text-sm font-medium leading-6 text-gray-900">
-                                 Status
-                              </label>
-                              {!true ? (
-                                 <p className="p-3 mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 tracking-wider">
-                                    Verified
-                                 </p>
-                              ) : (
+                        <div className="py-6 grid sm:grid-cols-3 gap-y-1 sm:gap-4 sm:px-0 sm:items-center">
+                           <h3 className="text-sm font-medium leading-6 text-gray-900">Status</h3>
+                           {!true ? (
+                              <p className="p-3 mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 tracking-wider">
+                                 Verified
+                              </p>
+                           ) : (
                                  <Button
                                     variant="text"
                                     color="primary"
@@ -165,9 +128,8 @@ const Profile = () => {
                                  >
                                     Subscribe now
                                  </Button>
-                              )}
-                           </div>
-                        )}
+                           )}
+                        </div>
                      </div>
                   </div>
                </div>
